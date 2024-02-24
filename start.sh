0dca507ce7e1b1ab6f72254fb34d225203f88709
poudrefr <128908094+poudrefr@users.noreply.github.com>
2024-02-24 18:08:09 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo apt-get install fonts-powerline
+sudo apt install fonts-powerline
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
