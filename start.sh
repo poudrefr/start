cd5ced91c3e7d996a406abdde6ab37bc1dad02a6
poudrefr <128908094+poudrefr@users.noreply.github.com>
2024-02-24 17:47:58 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,12 +7,12 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
-# rm -fr ~/.zshrc
-# cp ~/start/.zshrc ~/
-# rm -fr ~/powerlevel10k/config/p10k-lean.zsh
-# cp ~/start/p10k-lean.zsh ~/powerlevel10k/config/
-# rm -fr ~/.p10k.zsh
-# cp ~/start/.p10k.zsh ~/
+rm -fr ~/.zshrc
+cp ~/start/.zshrc ~/
+rm -fr ~/powerlevel10k/config/p10k-lean.zsh
+cp ~/start/p10k-lean.zsh ~/powerlevel10k/config/
+rm -fr ~/.p10k.zsh
+cp ~/start/.p10k.zsh ~/
 
 sudo apt install nodejs npm
 npm install @ibm/plex
