2526aa40e2594a380e08d2c5ce52a21f407e9671
poudrefr <128908094+poudrefr@users.noreply.github.com>
2024-03-20 15:49:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,19 +10,14 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
-rm -fr ~/.zshrc
+
 cp ~/start/.zshrc ~/
-# rm -fr ~/powerlevel10k/config/p10k-lean.zsh
 cp ~/start/p10k-lean.zsh ~/powerlevel10k/config/
-rm -fr ~/.p10k.zsh
 cp ~/start/.p10k.zsh ~/
 
 sudo apt install fonts-ibm-plex
-# sudo apt install nodejs npm
-# npm install @ibm/plex
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-#touch ~/.vim_runtime/my_configs.vim
 echo "set number" >> ~/.vim_runtime/my_configs.vim
 
 source .zshrc
